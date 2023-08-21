b54862b097bb2452244593dbdb38719388174d9a
Pilli Soso <pillisoso@gmail.com>
2023-08-21 21:52:45 +0600

    Add files via upload

---
+++ py_ch_12_webScrp/py_ch12_2_2_webScrap_BeutSup.py
@@ -0,0 +1,18 @@
+
+# Courses: colt_py_bootcamps    327
+
+# ----------------    Beautiful soup    ----------------
+
+# INSTALLING Beautiful soup
+# pip install bs4
+# This is a dummy package managed by the developer of Beautiful Soup to prevent name squatting. 
+    # The official name of PyPI’s Beautiful Soup Python package is "beautifulsoup4". 
+    # This package ensures that if you type 'pip install bs4' by mistake you will end up with Beautiful Soup.
+
+# Instead use:
+    # pip install beautifulsoup4
+
+# Beautiful Soup is a library that makes it easy to scrape information from web pages. 
+    # It sits atop an HTML or XML parser, 
+    # roviding Pythonic idioms for iterating, searching, and modifying the parse tree.
+
